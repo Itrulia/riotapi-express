0a1e4798db1dcb647efde729d7bcdcb2e3a8de5d
Karl Merkli <info@karl-merkli.ch>
2016-04-19 19:21:48 +0200

    Regenerated API key and hidden it

--- gnarFactory.js
+++ gnarFactory.js
@@ -1,9 +1,10 @@
 var gnar = require('./gnar');
+var env = require('./.env.json');
 
 module.exports = function(region) {
     if (!region) {
         region = 'EUW';
     }
 
-    return gnar('625d373a-a598-41f8-b088-c61cfb804a37', region.toLowerCase());
+    return gnar(env.api_key, region.toLowerCase());
 };
